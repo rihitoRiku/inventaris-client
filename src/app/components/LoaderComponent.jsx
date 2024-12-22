237afeb2be68fc61c64e3e32c6d5e2d99822af9c
darkRihito <fieshidiq@gmail.com>
2024-12-23 01:56:02 +0700

    Minor bug fixed

--- src/app/components/LoaderComponent.jsx
+++ src/app/components/LoaderComponent.jsx
@@ -6,7 +6,7 @@ import Loaderstyle2 from "./../styles/LoaderStyle2.module.scss";
 export function Loadercomponent() {
   return (
     <>
-      <div className="top-0 start-0 w-screen h-screen z-40 bg-white/10 backdrop-blur-sm fixed bg-white grid place-items-center">
+      <div className="top-0 start-0 w-screen h-screen z-[100] bg-white/10 backdrop-blur-sm fixed bg-white grid place-items-center">
         <div
           className={`dark:text-green-600 text-green-300 ${Loaderstyle.loader}`}
         ></div>
